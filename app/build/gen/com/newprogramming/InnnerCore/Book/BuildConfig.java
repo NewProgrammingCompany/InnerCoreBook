cf8def70b78036a11b7485d21ad7cbb90ca0129c
root <root@localhost.localdomain>
2018-06-18 18:28:09 +0500

    commiting by TECKTONIK1337

---
+++ app/build/gen/com/newprogramming/InnnerCore/Book/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.newprogramming.InnnerCore.Book;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ app/src/main/java/com/newprogramming/InnnerCore/Book/MainActivity.java
@@ -0,0 +1,28 @@
+package com.newprogramming.InnnerCore.Book;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+	public void fuc(View v){
+		setContentView(R.layout.main1);}
+	public void fuc1(View v){
+		setContentView(R.layout.main);}
+	public void uroki(View v){
+		setContentView(R.layout.uroki);}
+	public void s(View v){
+		setContentView(R.layout.s);}
+	public void n(View v){
+		setContentView(R.layout.main);}
+	public void y(View v){
+		setContentView(R.layout.s1);}
+	public void s1(View v){
+		setContentView(R.layout.s2);}}
